3ba6f967f3e34d33f6f9d17dd7d98c7d277229f6
ismail <126421146+ismail-aykurt@users.noreply.github.com>
2023-04-14 01:30:27 +0300

    Add files via upload

---
+++ MaasHesaplama/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee employee = new Employee("ISMAIL", 2000,45,1985);
+        String yazdir = employee.toString();
+        System.out.println(yazdir);
+    }
+}
